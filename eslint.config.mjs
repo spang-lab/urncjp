50864435e3f8ce7a26c6f026796e99581a816d15
Michael Huttner <michael@mhuttner.com>
2025-03-27 15:21:35 +0100

    fuck prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,22 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier/recommended';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  prettier,
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
         {
-          args: 'none'
-        }
+          args: "none",
+        },
       ],
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
-  }
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  globalIgnores(["lib/**/*", "urncjp/labextension/static/**/*"]),
 );
